c3682c2b414f77462341dcfeec1e0811d2e808ba
jiyeon <alclsemfja@naver.com>
2021-09-30 14:39:37 +0900

    dept자바프로젝트

--- jdbc/src/dept/DeptDTO.java
+++ jdbc/src/dept/DeptDTO.java
@@ -1,5 +1,43 @@
 package dept;
+//dto는 테이블 모양과 같이 만든다 
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Setter  
+@Getter  
+@NoArgsConstructor 
+@AllArgsConstructor 
+@ToString
+
 
 public class DeptDTO {
+	
+	private String deptno;
+	private String dname;
+	private String loc;
 
+	//생성자 ,getter,setter,toString만들기
+
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- jdbc/src/dept/DeptMain.java
+++ jdbc/src/dept/DeptMain.java
@@ -3,8 +3,10 @@ package dept;
 public class DeptMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+  
+		
+		
+		
 	}
 
 }
